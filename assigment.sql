ef530fd5a753a457b0962daa0657c1005300f41c
shubho-123 <114208046+shubho-123@users.noreply.github.com>
2022-12-21 12:09:42 +0530

    Add files via upload

---
+++ assigment.sql
@@ -0,0 +1,16 @@
+create database database1;
+
+use database1;
+
+create table student(
+roll int primary key,
+name varchar(50),
+contact varchar(15),
+city varchar(50),
+email varchar(50),
+standard varchar(50)
+);
+
+select * from student;
+
+select * from User;
